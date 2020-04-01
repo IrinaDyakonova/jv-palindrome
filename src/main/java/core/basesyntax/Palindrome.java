0d216849a4e0df0dc60b430dd0c063f1587e3c57
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-04-01 17:50:30 +0300

    hw_3_3

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,6 +21,8 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        text = text.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        StringBuffer result = new StringBuffer(text);
+        return text.equals(result.reverse().toString());
     }
 }
